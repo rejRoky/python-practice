b8bfd0c97e000dcd69fbdd9195abee59ca3b72c3
rejRoky <rejaul.islam.roky@gmail.com>
2022-05-25 03:25:54 +0600

    Basic Python updated

--- Basic Python/BasicCode.py
+++ Basic Python/BasicCode.py
@@ -6,7 +6,7 @@ print(' ||||')
 # String Multiple
 print ('*' * 10)
 
-#Variable
+#Variable & Datatype
 price = 10      #integer
 price = 12.3    #float
 name = 'Roky'   #string
@@ -21,6 +21,30 @@ print (price)
 name = input ("Enter Your name : ")
 print ('Hello, ' + name + ' !!')
 
+# Datatype conversion
+birth_Year = input ("Enter Your Birth Year :")
+print (type(birth_Year))
+
+age = 2022 - int(birth_Year)  # Type conversion Str to int 
+print (type(age))
+print ('You are now ' + str(age)) # Type conversion  int to str
+
+
+#Strings 
+jobTitle = 'I am  a "System Engineer"'
+print (jobTitle)
+print (jobTitle[3: ]) #printing by index position Sqer b []
+print (jobTitle[-1]) #printing by index position
+new = jobTitle [:]
+print(new)
+
+#Muliline string 
+aboutMe ='''
+    I am  a "System Engineer". 
+    I live in Bangladesh.
+    i am now 26.
+    '''
+print(aboutMe)
 
 
 
