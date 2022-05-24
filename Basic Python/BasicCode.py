4880341a7318f24dc8c538f304b5c6ee6c094f55
rejRoky <rejaul.islam.roky@gmail.com>
2022-05-25 01:22:12 +0600

    Basic Python update

--- Basic Python/BasicCode.py
+++ Basic Python/BasicCode.py
@@ -1,2 +1,28 @@
-print ("Hello, Python");
+#Hello Python
+print ("Hello, Python")
+print ('o----')
+print(' ||||')
+
+# String Multiple
+print ('*' * 10)
+
+#Variable
+price = 10      #integer
+price = 12.3    #float
+name = 'Roky'   #string
+is_busy = True  #boolen
+
+price = 20 
+print (price)
+price = 10.3
+print (price)
+
+# Input form user
+name = input ("Enter Your name : ")
+print ('Hello, ' + name + ' !!')
+
+
+
+
+
 
